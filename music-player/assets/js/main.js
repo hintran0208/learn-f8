3b8e5c9394802bedf14a6f153af8cc3b938ca477
Hien Tran <tranthanhhien5@gmail.com>
2021-08-13 16:19:20 +0700

    update render playlist

---
+++ music-player/assets/js/main.js
@@ -0,0 +1,67 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const app = {
+    songs: [
+        {
+            name: 'Muộn rồi mà sao còn',
+            singer: 'Sơn Tùng MTP',
+            path: '../music/song1.mp3',
+            image: './assets/img/song1.jpg'
+        },
+        {
+            name: 'Lặng nghe nước mắt',
+            singer: 'Mr Siro',
+            path: '../music/song2.mp3',
+            image: './assets/img/song2.jpg'
+        },
+        {
+            name: 'Tự lau nước mắt',
+            singer: 'Mr Siro',
+            path: '../music/song3.mp3',
+            image: './assets/img/song3.jpg'
+        },
+        {
+            name: 'Tình yêu chấp vá',
+            singer: 'Mr Siro',
+            path: '../music/song4.mp3',
+            image: './assets/img/song4.jpg'
+        },
+        {
+            name: 'Gương mặt lạ lẫm',
+            singer: 'Mr Siro',
+            path: '../music/song5.mp3',
+            image: './assets/img/song5.jpg'
+        }
+    ],
+
+    render: function () {
+        const htmls = this.songs.map(song => {
+            return `
+                <div class="song">
+                    <div
+                        class="thumb"
+                        style="
+                        background-image: url('${song.image}');
+                        "
+                    ></div>
+                    <div class="body">
+                        <h3 class="title">${song.name}</h3>
+                        <p class="author">${song.singer}</p>
+                    </div>
+                    <div class="option">
+                        <i class="fas fa-ellipsis-h"></i>
+                    </div>
+                </div>
+            `;
+        })
+        $('.playlist').innerHTML = htmls.join('');
+
+    },
+
+    start: function () {
+        this.render();
+    }
+}
+
+app.start();
